70e1a1b1b8c85f09f8a19ed727074b96673b193a
Moises Berrospi <mbf150@gmail.com>
2022-09-19 00:52:50 -0500

    THEBOSSweb

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var port = process.env.port || 4201;
+
+var app = express();
+
+var cliente_router = require('./routes/cliente');
+var usuario_router = require('./routes/usuario');
+
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyparser.json({limit: '50mb', extended: true}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/tienda',(err,res)=>{
+   if(err){
+        console.log(err);
+    }else{
+        app.listen(port,function(){
+            console.log('Servidor corriendo '+port);
+        });
+        
+    }
+});
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*'); 
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/api',cliente_router);
+app.use('/api',usuario_router);
+
+module.exports = app;
---
+++ backend/controllers/clienteController.js
@@ -0,0 +1,7 @@
+const testing = async function(req,res){
+    res.status(200).send('Hola test api');
+}
+
+module.exports = {
+    testing
+}
