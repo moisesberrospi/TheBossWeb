2071b40d32d70d70a3604c352f0bc1415ee7b1d2
Moises Berrospi <mbf150@gmail.com>
2022-10-23 13:56:33 -0500

    Añadir Sidebar

--- tienda/src/views/cuenta/DireccionesApp.vue
+++ tienda/src/views/cuenta/DireccionesApp.vue
@@ -118,27 +118,7 @@
             </div>
             <!-- Customer Sidebar-->
             <div class="col-xl-3 col-lg-4 mb-5">
-                <div class="customer-sidebar card border-0"> 
-                <div class="customer-profile"><a class="d-inline-block" href="#"><img class="img-fluid rounded-circle customer-image shadow" src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/photo/kyle-loftus-589739-unsplash-avatar.jpg" alt=""></a>
-                    <h5>Julie Trnka</h5>
-                    <p class="text-muted text-sm mb-0">Ostrava, Czech Republic</p>
-                </div>
-                <nav class="list-group customer-nav"><a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-orders.html"><span>
-                        <svg class="svg-icon svg-icon-heavy me-2">
-                        <use xlink:href="#paper-bag-1"> </use>
-                        </svg>Orders</span>
-                    <div class="badge rounded-pill bg-dark fw-normal px-3">5</div></a><a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-account.html"><span>
-                        <svg class="svg-icon svg-icon-heavy me-2">
-                        <use xlink:href="#male-user-1"> </use>
-                        </svg>Profile</span></a><a class="active list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-addresses.html"><span>
-                        <svg class="svg-icon svg-icon-heavy me-2">
-                        <use xlink:href="#navigation-map-1"> </use>
-                        </svg>Addresses</span></a><a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-login.html"><span>
-                        <svg class="svg-icon svg-icon-heavy me-2">
-                        <use xlink:href="#exit-1"> </use>
-                        </svg>Log out</span></a>
-                </nav>
-                </div>
+               <SidebarCliente/>
             </div>
             <!-- /Customer Sidebar-->
             </div>
@@ -149,6 +129,8 @@
 </template>
 <script>
 import axios from 'axios';
+import SidebarCliente from '@/components/SidebarCliente.vue' 
+
 export default {
     data() {
         return {
@@ -158,6 +140,9 @@ export default {
             msm_error:'',
             direcciones: []
         }
+    },
+    components:{
+       SidebarCliente 
     },
     beforeMount() {
         this.init_direcciones();
